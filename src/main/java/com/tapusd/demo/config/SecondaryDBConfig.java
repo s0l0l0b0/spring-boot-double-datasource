9aa7a43186f1b7cc694cbd876c4876c8aed535c0
T4puSD <sdtapusd20@gmail.com>
2024-12-29 16:45:48 +0600

    feat(druid): add multi datasource using druid

--- src/main/java/com/tapusd/demo/config/SecondaryDBConfig.java
+++ src/main/java/com/tapusd/demo/config/SecondaryDBConfig.java
@@ -1,59 +1,43 @@
 package com.tapusd.demo.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.JdbcTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 public class SecondaryDBConfig {
 
-    @Value("${secondary.datasource.url}")
-    private String url;
-
-    @Value("${secondary.datasource.username}")
-    private String username;
-
-    @Value("${secondary.datasource.password}")
-    private String password;
-
-    @Value("${secondary.datasource.driver-class-name}")
-    private String driverClassName;
+    @Primary
+    @Bean(name = "dataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.druid.one")
+    public DataSource dataSource() {
+        return DruidDataSourceBuilder.create().build();
+    }
 
     @Bean(name = "secondaryDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.druid.two")
     public DataSource secondaryDataSource() {
-        HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(url);
-        hikariConfig.setUsername(username);
-        hikariConfig.setPassword(password);
-        hikariConfig.setDriverClassName(driverClassName);
-        hikariConfig.setMinimumIdle(5);
-        hikariConfig.setMaximumPoolSize(20);  // Set the max pool size, you can adjust based on your needs
-        hikariConfig.setPoolName("SecondaryHikariPool");
+        return DruidDataSourceBuilder.create().build();
+    }
 
-        // Additional HikariCP properties
-        hikariConfig.setConnectionTimeout(30000);  // Connection timeout in milliseconds
-        hikariConfig.setIdleTimeout(600000);  // Idle timeout in milliseconds (10 minutes)
-        hikariConfig.setMaxLifetime(1800000);  // Max lifetime of a connection (30 minutes)
-
-        return new HikariDataSource(hikariConfig);
+    @Primary
+    @Bean(name = "jdbcTemplate")
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(secondaryDataSource());
     }
 
     @Bean(name = "secondaryJdbcTemplate")
     public JdbcTemplate secondaryJdbcTemplate() {
         return new JdbcTemplate(secondaryDataSource());
-    }
-
-    @Bean(name = "secondaryTransactionManager")
-    public PlatformTransactionManager secondaryTransactionManager() {
-        return new JdbcTransactionManager(secondaryDataSource());
     }
 }
